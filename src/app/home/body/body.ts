b5cf358e3e29fd49bacd9de3fc16d17d5560ff5c
juliand-07 <juliand_murcia@outlook.com>
2026-02-11 17:20:18 -0500

    crear contacto

--- src/app/home/body/body.ts
+++ src/app/home/body/body.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Profile } from './profile/profile';
 import { Proyects } from './proyects/proyects';
+import { Contact } from './contact/contact';
 
 
 @Component({
   selector: 'app-body',
-  imports: [Profile,Proyects],
+  imports: [Profile,Proyects,Contact],
   templateUrl: './body.html',
   styleUrl: './body.scss',
 })
